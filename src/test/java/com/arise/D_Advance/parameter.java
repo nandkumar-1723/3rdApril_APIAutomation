96710f8a6f57612c0697b101c132464cb80a7e20
nandkumar-1723 <127601507+nandkumar-1723@users.noreply.github.com>
2023-08-05 19:48:08 +0530

    for 3rd April Batch

---
+++ src/test/java/com/arise/D_Advance/parameter.java
@@ -0,0 +1,65 @@
+package com.arise.D_Advance;
+
+import com.arise.Utils.*;
+import io.restassured.*;
+import io.restassured.http.*;
+import io.restassured.response.*;
+import org.json.*;
+import org.json.simple.parser.*;
+import org.testng.annotations.*;
+
+import java.io.*;
+
+/**
+ * @author Nandkumar Babar
+ */
+public class parameter {
+
+        public static String cookieValue = "";
+        public static String issueId = "";
+
+        @Test(priority = 1)
+        public void loginToJira() throws IOException, ParseException {
+            String reqBody = ReadInputs.readJsonInputs("src/main/java/com/arise/InputJson/loginReqBody.json");
+            Response response = RestAssured.given().baseUri(ReadPropertiesFile.readProperties("URL")).body(reqBody).contentType(ContentType.JSON)
+                    .when().post("/rest/auth/1/session")
+                    .then().log().body().extract().response();
+
+            System.out.println(response.asString());
+            System.out.println(response.getStatusCode());
+            JSONObject js = new JSONObject(response.asString());
+            cookieValue = "JSESSIONID=" + js.getJSONObject("session").get("value").toString();
+            System.out.println(cookieValue);
+        }
+        @Test(priority = 2)
+        public void cretaeUserStory() throws IOException, ParseException {
+
+            String requestBody = ReadInputs.readJsonInputs("src/main/java/com/arise/InputJson/CreateUserStoryBody.json");
+            JSONObject updatedRB = new JSONObject(requestBody);
+            updatedRB.getJSONObject("fields").getJSONObject("issuetype").put("name","Story");
+
+            Response response = RestAssured.given().baseUri(ReadPropertiesFile.readProperties("URL")).body(updatedRB.toString()).contentType(ContentType.JSON).header("Cookie", cookieValue)
+                    .when().post("/rest/api/2/issue")
+                    .then().log().body().extract().response();
+
+            JSONObject js = new JSONObject(response.asString());
+            issueId = js.get("key").toString();
+            System.out.println(response.getStatusCode());
+            System.out.println(response.asString());
+
+        }
+
+    @Test(priority = 3)
+    public void getCreatedUserStory() throws IOException {
+        Response response = RestAssured.given().baseUri(ReadPropertiesFile.readProperties("URL")).contentType(ContentType.JSON).header("Cookie", cookieValue)
+                .pathParam("StoryId",issueId)
+                .queryParam("fields","status")
+                .queryParam("fields","priority")
+                .when().get("/rest/api/2/issue/{StoryId}")
+                .then().log().body().extract().response();
+        System.out.println(response.getStatusCode());
+        System.out.println(response.asString());
+
+//Please let me know once you will get the idea about collection
+    }
+    }
